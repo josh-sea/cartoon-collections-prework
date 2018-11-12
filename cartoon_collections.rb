9c0e129ad82df6bd9511b01747bbbb5abc71f2ae
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-12 14:55:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  for i in 0..array.length 
+    puts "#{i+1}. #{array[i]}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect do |i|
+    i = "#{i.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? {|i|i.length > 4}
 end
 
 def find_the_cheese# code an argument here
