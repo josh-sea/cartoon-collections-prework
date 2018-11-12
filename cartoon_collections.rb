7763a264db1a163b81866457fb08d29dea8821e7
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-12 17:47:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,10 +19,10 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  for i in 0...array.length
-    cheese_types.find do |j|
-      if array[i] == j
-        return array[i]
+  array.find do |i|
+    cheese_types.collect do |j|
+      if i == j
+        return i
       end 
     end
     return nil
